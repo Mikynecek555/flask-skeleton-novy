47fcb2beb1ebeb7a1bf1fbbd1b0401bcad9a8b13
Martin <Mikynecek555@gmail.com>
2020-09-29 10:18:01 +0200

    graf.tmpl views_mikes.py updated

--- src/public/views_mikes.py
+++ src/public/views_mikes.py
@@ -32,9 +32,11 @@ def formular():
     form = advancedform()
     if form.validate_on_submit():
         if form.oo.data == "2" and form.obrazec.data == "1":
-            return str(form.a.data*form.a.data)
+            hodnota=str(form.a.data*form.a.data)
+            return render_template("public/vystuphodnoty.tmpl", hodnota = hodnota)
         if form.oo.data == "2" and form.obrazec.data == "2":
-            return str(form.a.data*form.b.data)
+            hodnota=str(form.a.data*form.b.data)
+            return render_template("public/vystuphodnoty.tmpl", hodnota = hodnota)
         if form.oo.data == "2" and form.obrazec.data == "3":
             return str(((form.a.data)*math.sqrt(form.a.data*form.a.data-(form.c.data*form.c.data)/2))/2)
         if form.oo.data == "1" and form.obrazec.data == "1":
@@ -46,3 +48,13 @@ def formular():
         else:
             return "spatne"
     return render_template ('public/advancedform.tmpl',form = form)
+
+
+@blueprint.route("/simple_chart")
+def chart():
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('public/graf.tmpl', values=values, labels=labels, legend=legend)
+
+
